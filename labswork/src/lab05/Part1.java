c784253750c1ea73a1a1a92bf68e3b4b4938ae47
c0527253 <gwl10315179797@gmail.com>
2023-07-07 12:18:41 -0700

    lab5,p1 min method ok

--- labswork/src/lab05/Part1.java
+++ labswork/src/lab05/Part1.java
@@ -2,9 +2,19 @@ package lab05;
 
 import lab2.Rational;
 
-public class Part1 {
-    public static Rational smallestValue() {
-        return null;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Part1<E> implements Comparator<Rational> {
+
+    public static Rational minOne(List<Rational> rationals) {
+        System.out.println("test for method minOne ");
+        return Collections.min(rationals);
     }
 
+    @Override
+    public int compare(Rational o1, Rational o2) {
+        return o1.compareTo(o2);
+    }
 }
